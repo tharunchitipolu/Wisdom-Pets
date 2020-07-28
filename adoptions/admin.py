89d672da462ee3ab54d8caa0114fd56111ffd76b
chitipolu tharun <44120739+tharunchitipolu@users.noreply.github.com>
2020-07-28 20:30:14 +0530

    Add files via upload

---
+++ adoptions/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species', 'breed', 'age', 'sex']
